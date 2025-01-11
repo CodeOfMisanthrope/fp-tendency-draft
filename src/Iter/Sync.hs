3d86cc3a5ab5c12813c7ff19051abfb2790de7a2
Ilya <iosdsbb@gmail.com>
2025-01-11 21:44:55 +0300

    feat: added iFilter Iter

--- src/Iter/Sync.hs
+++ src/Iter/Sync.hs
@@ -1,6 +1,7 @@
 module Iter.Sync
   ( iTake,
     iEnumerate,
+    iFilter
   )
 where
 
@@ -12,3 +13,9 @@ iTake n (x : xs) = x : iTake (n - 1) xs
 iEnumerate :: Int -> [a] -> [(a, Int)]
 iEnumerate _ [] = []
 iEnumerate count (x : xs) = (x, count) : iEnumerate (count + 1) xs
+
+iFilter :: (a -> Bool) -> [a] -> [a]
+iFilter _ [] = []
+iFilter fn (x:xs) = if fn x
+  then x : iFilter fn xs
+  else iFilter fn xs
--- src/Lib.hs
+++ src/Lib.hs
@@ -3,12 +3,15 @@ module Lib
   )
 where
 
-import Iter.Sync (iEnumerate, iTake)
+import Iter.Sync (iEnumerate, iTake, iFilter)
 
 takedList = iTake 3 [1 .. 10]
-enumeratedList = iEnumerate 0 [1 .. 10]
+enumeratedList = iEnumerate 1 [1 .. 10]
+
+filteredList = iFilter (\x -> x > 2) [1..10]
 
 someFunc :: IO ()
 -- someFunc = putStrLn "someFunc"
 --someFunc = putStrLn (show takedList)
-someFunc = putStrLn (show enumeratedList)
+--someFunc = putStrLn (show enumeratedList)
+someFunc = putStrLn (show filteredList)
