7f84bcab028f69cbb950c9d19b5c0af1f0d49395
Ilya <iosdsbb@gmail.com>
2025-01-12 19:07:06 +0300

    docs: added desc for container Option

--- src/Containers/Option.hs
+++ src/Containers/Option.hs
@@ -26,5 +26,40 @@ module Containers.Option
   )
 where
 
+-- | The 'IOption' type represents an optional value that can either contain a value of type 'a'
+-- or indicate the absence of a value. This is analogous to the 'Option' type in Rust.
+--
+-- The 'IOption' type has two constructors:
+--
+-- * 'Some a': Represents the presence of a value of type 'a'.
+-- * 'None': Represents the absence of a value.
+--
+-- Example Usage:
+--
+-- >>> let value1 = Some 42
+-- >>> let value2 = None
+-- >>> value1
+-- Some 42
+-- >>> value2
+-- None
+-- >>> case value1 of
+-- ...   Some v -> "Contains: " ++ show v
+-- ...   None    -> "No value"
+-- "Contains: 42"
+--
+-- >>> case value2 of
+-- ...   Some v -> "Contains: " ++ show v
+-- ...   None    -> "No value"
+-- "No value"
+--
+-- The 'IOption' type can also be used in functions that require optional parameters.
+--
+-- Example Function:
+--
+-- >>> processOption :: IOption Int -> String
+-- >>> processOption (Some 10)
+-- "Contains value: 10"
+-- >>> processOption None
+-- "Does not contain a value"
 data IOption a = None | Some a
   deriving (Show, Eq)
